9593ed962d178e486092259756b4f56aeb169eee
GuilletJoseph <guilletjoseph85@gmail.com>
2022-10-15 12:16:11 +0200

    ff2

--- src/fr/declaration/variable/DeclarationApp.java
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -2,4 +2,31 @@ package fr.declaration.variable;
 
 public class DeclarationApp {
 
+	
+	
+	public static void main( String [] args) {
+		
+		byte b=2;
+		short s=4;
+		int i=5;
+		long l=78;
+		float f=1.25f;
+		double d=59.36;
+		char c='A';
+		boolean bo=false;
+		String st="Hello";
+		String randomString = "Voici le résultat d’un calcul :" + "\n" +"1+5=6";	
+		System.out.println( b);	
+		System.out.println( s);	
+		System.out.println( i);		
+		System.out.println( l);	
+		System.out.println( f);	
+		System.out.println( d);	
+		System.out.println( c);	
+		System.out.println( st+"=Bonjour");	
+		System.out.println(randomString);
+
+	
+	
+	}
 }
